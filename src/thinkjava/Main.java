e1b7eb79e1752bddd1ba5c1db611218fd202f808
Lemme Velleõu <Lemme Velleu>
2016-10-18 21:08:24 +0300

    Initial commit

---
+++ src/thinkjava/Main.java
@@ -0,0 +1,14 @@
+package thinkjava;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        //Add a second line of code to the program
+
+        System.out.print("How are you?");
+
+    }
+}
